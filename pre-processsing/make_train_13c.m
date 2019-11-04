a9219ab3bea724414a2f4e8bbc127413a4f42f42
Xinyang <xylithu@gmail.com>
2019-11-04 21:05:06 +0800

    Add files via upload

---
+++ pre-processsing/make_train_13c.m
@@ -0,0 +1,75 @@
+clc;clear;
+
+patch_size = 256;
+nb_per_slide = 1000;
+
+folderA = './raw/trainA-all/';
+folderB = './raw/trainB-all/';
+%% for A
+list = dir([folderA '*.tif']);
+k = 0;
+for ii = 1:length(list)
+    
+    fprintf('%d of %d...\n', ii, length(list))
+    img = loadtiff(fullfile(list(ii).folder, list(ii).name));
+    
+    [xs,ys,~] = size(img);
+    nb_per_slide = floor(xs*ys/7000);
+    
+    % cord
+    cord = zeros((xs-patch_size+1)*(ys-patch_size+1), 2);
+    for i = 1:(xs-patch_size+1)
+        for j = 1:(ys-patch_size+1)
+            
+            cord(j+(i-1)*(ys-patch_size+1), 1) = i;
+            cord(j+(i-1)*(ys-patch_size+1), 2) = j;
+            
+        end
+    end
+    
+    %
+    sp = randperm(size(cord, 1), nb_per_slide);
+    for j = 1:nb_per_slide
+        k=k+1;
+        xi = cord(sp(j), 1);
+        yi = cord(sp(j), 2);
+        imgP = img(xi:xi+patch_size-1,yi:yi+patch_size-1,:);
+        
+        saveastiff(imgP, ['./trainA/' num2str(k) '.tif']);
+    end
+end
+
+
+%% for B
+list = dir([folderB '*.png']);
+k = 0;
+for ii = 1:length(list)
+    
+    fprintf('%d of %d...\n', ii, length(list))
+    img = imread(fullfile(list(ii).folder, list(ii).name));
+    
+    [xs,ys,~] = size(img);
+    nb_per_slide = floor(xs*ys/7000);
+    
+    % cord
+    cord = zeros((xs-patch_size+1)*(ys-patch_size+1), 2);
+    for i = 1:(xs-patch_size+1)
+        for j = 1:(ys-patch_size+1)
+            
+            cord(j+(i-1)*(ys-patch_size+1), 1) = i;
+            cord(j+(i-1)*(ys-patch_size+1), 2) = j;
+            
+        end
+    end
+    
+    %
+    sp = randperm(size(cord, 1), nb_per_slide);
+    for j = 1:nb_per_slide
+        k=k+1;
+        xi = cord(sp(j), 1);
+        yi = cord(sp(j), 2);
+        imgP = img(xi:xi+patch_size-1,yi:yi+patch_size-1,:);
+        
+        imwrite(imgP, ['./trainB/' num2str(k) '.tif']);
+    end
+end
